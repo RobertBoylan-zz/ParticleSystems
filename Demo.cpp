96c99b59aa6fcfef3d0666736ff18be76849923d
RobertBoylan <RobertBoylan@hotmail.com>
2017-04-09 17:41:02 +0100

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -3,7 +3,9 @@
 Demo::Demo() {
 
 	gravity = -9.81f;
-	pause = false;
+	particlesPerSecond = 1000.0f;
+	cameraPosition = fountainDirection = vec3(0.0f, 0.0f, 0.0f);
+	pause = whiteBackground = false;
 }
 
 int Demo::CreateDemoWindow(int _mode) {
@@ -49,10 +51,8 @@ void Demo::DemoDisplay() {
 
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window)) {
 		
-		particle.DrawParticles(window, mode, gravity, pause);
-		PauseDemo();
-		ChangeParameter();
-		PickDemo();
+		particle.DrawParticles(window, mode, gravity, particlesPerSecond, fountainDirection, pause, whiteBackground);
+		KeyboardOperations();
 		
 		/* Swap buffers */
 		glfwSwapBuffers(window);
@@ -65,62 +65,125 @@ void Demo::CleanUp() {
 	particle.CleanUp(window);
 }
 
-void Demo::ChangeParameter() {
+void Demo::KeyboardOperations() {
 
 	if (glfwGetKey(window, GLFW_KEY_KP_ADD) == GLFW_PRESS) {
 
-		if (mode == FOUNTAIN_DEMO && gravity <= 15.0f) {
+		if (gravity <= 15.0f) {
 
 			gravity += 0.02f;
 		}
 	}
+
 	if (glfwGetKey(window, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS) {
 
-		if (mode == FOUNTAIN_DEMO && gravity >= -100.0f) {
+		if (gravity >= -100.0f) {
 
 			gravity -= 0.02f;
 		}
 	}
-}
 
-void Demo::PauseDemo() {
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+
+		if (fountainDirection.z <= 4.0f) {
+
+			fountainDirection.z += 0.01f;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+
+		if (fountainDirection.z >= -4.0f) {
+
+			fountainDirection.z -= 0.01f;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+
+		if (fountainDirection.x <= 4.0f) {
+
+			fountainDirection.x += 0.01f;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+
+		if (fountainDirection.x >= -4.0f) {
+
+			fountainDirection.x -= 0.01f;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS) {
+
+		if (particlesPerSecond > 0.0f) {
+			
+			particlesPerSecond -= 10.0f;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
+
+		if (particlesPerSecond < 15000.0f) {
+
+			particlesPerSecond += 10.0f;
+		}
+	}
 
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE) {
 
 		pause = false;
 	}
+
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
 
 		pause = true;
 	}
-}
 
-void Demo::PickDemo() {
+	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) {
+
+		whiteBackground = !whiteBackground;
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+
+		particlesPerSecond = 1000.0f;
+		gravity = -9.81f;
+		fountainDirection = vec3(0.0f, 0.0f, 0.0f);
+		whiteBackground = false;
+	}
 
 	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
 
 		mode = SNOW_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
 
 		mode = RAIN_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
 
 		mode = STARS_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS) {
 
 		mode = CLOUD_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS) {
 
 		mode = FOUNTAIN_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_6) == GLFW_PRESS) {
 
 		mode = SMOKE_DEMO;
 	}
+
 	else if (glfwGetKey(window, GLFW_KEY_7) == GLFW_PRESS) {
 
 		mode = FIRE_DEMO;
